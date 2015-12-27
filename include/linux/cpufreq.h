86ef021ef66f7b41962d1f30c299657b29f8f674
brymaster5000 <brymaster5000@gmail.com>
2015-12-26 20:02:27 -0500

    cpufreq: initial commit for smartmax from maxwen
    Conflicts:
    	drivers/cpufreq/Kconfig
    	include/linux/cpufreq.h
    
    Conflicts:
    	drivers/cpufreq/Kconfig
    	drivers/cpufreq/Makefile
    	include/linux/cpufreq.h
    
    smartmax: initial tweak for Shamu tunables
    
    incremental smartmax changes
    
    Conflicts:
    	arch/arm/configs/shamu_defconfig
    
    Conflicts:
    	arch/arm/configs/shamu_defconfig
    
    smartmax fixes
    
    tweak tuneables again
    
    smartmax: delete unsigned
    
    tweak smartmax tuneables
    
    tuneables tweak again
    
    Smartmax: These tuneables are the shit
    
    one last attempt
    
    smartmax: last remaining power_suspend switches
    
    cpufreq: add PMC governor
    
    cpufreq: Alter up Smartmax for PMC
    
    Conflicts:
    	drivers/cpufreq/Makefile
    
    Enable smartmax and add file that got missed in a pick

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -223,6 +223,8 @@ struct cpufreq_driver {
 	unsigned int	(*get)	(unsigned int cpu);
 
 	/* optional */
+	unsigned int (*getavg)	(struct cpufreq_policy *policy,
+                            unsigned int cpu);
 	int	(*bios_limit)	(int cpu, unsigned int *limit);
 
 	int	(*exit)		(struct cpufreq_policy *policy);
@@ -396,6 +398,8 @@ int cpufreq_driver_target(struct cpufreq_policy *policy,
 int __cpufreq_driver_target(struct cpufreq_policy *policy,
 				   unsigned int target_freq,
 				   unsigned int relation);
+extern int __cpufreq_driver_getavg(struct cpufreq_policy *policy,
+                                   unsigned int cpu);
 int cpufreq_register_governor(struct cpufreq_governor *governor);
 void cpufreq_unregister_governor(struct cpufreq_governor *governor);
 
@@ -427,6 +431,15 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LIONHEART)
 extern struct cpufreq_governor cpufreq_gov_lionheart;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_lionheart)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_NIGHTMARE)
+extern struct cpufreq_governor cpufreq_gov_nightmare;
+#define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_NIGHTMARE)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTMAX)
+extern struct cpufreq_governor cpufreq_gov_smartmax;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_smartmax)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_PMC)
+extern struct cpufreq_governor cpufreq_gov_pmc;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_pmc)
 #endif
 
 /*********************************************************************
