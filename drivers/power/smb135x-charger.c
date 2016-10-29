59d72378eab93e152e10e20664d68a0932c96d8f
TeamFahQ <teamfahq1@gmail.com>
2016-10-29 06:33:28 -0500

    fastcharge:code cleanup

--- drivers/power/smb135x-charger.c
+++ drivers/power/smb135x-charger.c
@@ -30,6 +30,10 @@
 #include <linux/reboot.h>
 #include <linux/qpnp/qpnp-adc.h>
 
+#ifdef CONFIG_FORCE_FAST_CHARGE
+#include <linux/fastcharge.h>
+#endif
+
 #define SMB135X_BITS_PER_REG	8
 
 /* Mask/Bit helpers */
@@ -1486,10 +1490,6 @@ static int smb135x_set_high_usb_chg_current(struct smb135x_chg *chip,
 	return rc;
 }
 
-#ifdef CONFIG_FORCE_FAST_CHARGE
-extern int force_fast_charge;
-#endif
-
 #define MAX_VERSION			0xF
 #define USB_100_PROBLEM_VERSION		0x2
 /* if APSD results are used
@@ -1539,19 +1539,14 @@ static int smb135x_set_usb_chg_current(struct smb135x_chg *chip,
 		goto out;
 	}
 	if (current_ma == CURRENT_500_MA) {
-
+		
 #ifdef CONFIG_FORCE_FAST_CHARGE
-		if (force_fast_charge) {
-			current_ma = CURRENT_900_MA;
-			rc = smb135x_masked_write(chip, CFG_5_REG,
-				USB_2_3_BIT, USB_2_3_BIT);
-		} else
-			rc = smb135x_masked_write(chip, CFG_5_REG,
-				USB_2_3_BIT, 0);
-#else
-		rc = smb135x_masked_write(chip, CFG_5_REG, USB_2_3_BIT, 0);
+		if (force_fast_charge)
+			rc = smb135x_masked_write(chip, CFG_5_REG, USB_2_3_BIT, USB_2_3_BIT);
+		else
 #endif
-
+			rc = smb135x_masked_write(chip, CFG_5_REG, USB_2_3_BIT, 0);
+	
 		rc |= smb135x_masked_write(chip, CMD_INPUT_LIMIT,
 				USB_100_500_AC_MASK, USB_500_VAL);
 		rc |= smb135x_path_suspend(chip, USB, CURRENT, false);
@@ -2183,14 +2178,7 @@ static void smb135x_external_power_changed(struct power_supply *psy)
 
 	if (chip->usb_psy_ma != current_limit) {
 		mutex_lock(&chip->current_change_lock);
-#ifdef CONFIG_FORCE_FAST_CHARGE
-		if (force_fast_charge)
-			chip->usb_psy_ma = 1600;
-		else
-			chip->usb_psy_ma = current_limit;
-#else
 		chip->usb_psy_ma = current_limit;
-#endif
 		rc = smb135x_set_appropriate_current(chip, USB);
 		mutex_unlock(&chip->current_change_lock);
 		if (rc < 0)
@@ -3330,7 +3318,7 @@ static int smb135x_setup_vbat_monitoring(struct smb135x_chg *chip)
 
 	if (chip->poll_fast) { /* the adc polling rate is higher*/
 		chip->vbat_monitor_params.timer_interval =
-			ADC_MEAS1_INTERVAL_250MS;
+			ADC_MEAS1_INTERVAL_31P3MS;
 	} else /* adc polling rate is default*/ {
 		chip->vbat_monitor_params.timer_interval =
 			ADC_MEAS1_INTERVAL_1S;
